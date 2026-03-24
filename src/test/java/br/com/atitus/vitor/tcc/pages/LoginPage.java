b14b49ed87cf5a59634e15ef75a8a022b15cd26d
Vitor Almeida Machado <almeida.vitor443@gmail.com>
2026-03-23 23:22:40 -0300

    test: implementa baseline manual (gabarito) para comparativo de IA

--- src/test/java/br/com/atitus/vitor/tcc/pages/LoginPage.java
+++ src/test/java/br/com/atitus/vitor/tcc/pages/LoginPage.java
@@ -1,4 +1,34 @@
 package br.com.atitus.vitor.tcc.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+    private WebDriver driver;
+
+    // Seletores baseados na Tabela 1 do Requisito
+    private By campoEmail = By.cssSelector("[data-test='email']");
+    private By campoSenha = By.cssSelector("[data-test='password']");
+    private By botaoEntrar = By.className("btn-primary");
+    private By mensagemErro = By.id("error-message");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void preencherEmail(String email) {
+        driver.findElement(campoEmail).sendKeys(email);
+    }
+
+    public void preencherSenha(String senha) {
+        driver.findElement(campoSenha).sendKeys(senha);
+    }
+
+    public void clicarEntrar() {
+        driver.findElement(botaoEntrar).click();
+    }
+
+    public String obterMensagemErro() {
+        return driver.findElement(mensagemErro).getText();
+    }
 }
---
+++ src/test/java/br/com/atitus/vitor/tcc/stepdefinitions/LoginSteps.java
@@ -0,0 +1,28 @@
+package br.com.atitus.vitor.tcc.stepdefinitions;
+
+import br.com.atitus.vitor.tcc.pages.LoginPage;
+import io.cucumber.java.pt.*; // Anotações em português conforme Gherkin
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertTrue;
+
+public class LoginSteps {
+    private WebDriver driver = new ChromeDriver();
+    private LoginPage loginPage = new LoginPage(driver);
+
+    @Dado("que eu estou na página de login")
+    public void que_eu_estou_na_pagina_de_login() {
+        driver.get("https://nexus-login-demo.com"); // URL fictícia do projeto
+    }
+
+    @Quando("eu insiro um e-mail inválido")
+    public void eu_insiro_um_e_mail_inválido() {
+        loginPage.preencherEmail("invalido.com");
+    }
+
+    @Entao("o sistema deve exibir uma mensagem de erro de formato")
+    public void o_sistema_deve_exibir_mensagem_erro() {
+        assertTrue(loginPage.obterMensagemErro().contains("E-mail inválido"));
+        driver.quit();
+    }
+}
